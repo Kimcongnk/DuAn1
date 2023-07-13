3571380b2e6ae8f560a55d57c1c13e054bfafbfd
Kimcongnk <congnkph29875@fpt.edu.vn>
2023-07-13 20:06:22 +0700

    comit

--- app/src/main/java/nhom1/fpoly/duan1/customer/fragment/HomeFragment.java
+++ app/src/main/java/nhom1/fpoly/duan1/customer/fragment/HomeFragment.java
@@ -7,15 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import nhom1.fpoly.duan1.R;
 
 public class HomeFragment extends Fragment {
-
+private TextView addCategory, addProduct;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        addCategory = view.findViewById(R.id.addDanhMuc);
+        addProduct = view.findViewById(R.id.addProduct);
+        addCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
     }
 }
